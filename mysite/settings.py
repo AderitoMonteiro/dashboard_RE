d713cf59298acd1c44e20ef0e24c44e9e62edd89
unknown <DGAPE@RAI-GV-DGAPE-81.gov.cv>
2025-08-21 12:11:03 -0100

    config BD ok

--- mysite/settings.py
+++ mysite/settings.py
@@ -79,8 +79,15 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'mssql',
+        'NAME': 'BDRE',
+        'USER': 'sa',
+        'PASSWORD': 'A07971.sql',
+        'HOST': '127.0.0.1',   # ou o IP/servidor
+        'PORT': '1433',        # porta padrão do SQL Server
+        'OPTIONS': {
+            'driver': 'ODBC Driver 17 for SQL Server',  # confirma a versão instalada
+        },
     }
 }
 
